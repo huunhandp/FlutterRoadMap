13322c121c3b758c0c77206a2bdfd2254c829717
huuNhan <nhandph1006@gmail.com>
2026-07-09 17:24:10 +0700

    cap nhap variables lan 2

---
+++ variables/bt2.dart
@@ -0,0 +1,22 @@
+/*
+Cach khai bao bien :
+- su dung tu khoa var : de dart tu suy luan kieu
+- khai bao voi kieu du lieu cu the : 
+- khai bao bang object
+ */
+
+void main() {
+  var name = "huuNhan"; // String
+  var age = 21;
+
+  // khai bao kieu du lieu cu the
+  String name1 = "Gyy nahn";
+  int age1 = 12;
+
+  // khai bao bang object
+  Object name2 = "HUu Nhan ";
+
+
+  // NUll safety
+  
+}
---
+++ variables/bt4-final-const.dart
@@ -0,0 +1,6 @@
+void main() {
+  final String name =
+      "Huu Nhan"; // khai bao kieu du lieu final ( chỉ được khai báo 1 lần )
+  const String name2 =
+      "Huu Nhan 2"; // khai bao du lieu kieu const ( khai bao xong phai gan gia tri ngay lap tuc )
+}
