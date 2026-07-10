b269e74de2b906f73cfd66c0f5973f75b7ebba22
huuNhan <nhandph1006@gmail.com>
2026-07-10 08:06:50 +0700

    up code builtInTypes lan 3

---
+++ builtInTypes/bt5-list.dart
@@ -0,0 +1,14 @@
+// Dinh nghia
+/*
+List la tap hop cac phan tu co thu tu va co the trung lap
+cac phan tu duọc truy cap thong qua index tu 0
+kich thuoc co the thay doi 
+ */
+
+void main() {
+  List<String> list1 = ['A', 'B', 'C'];
+  var list2 = [1, 2, 3];
+  List<String> list3 = [];
+  var list4 = List<int>.filled(3, 1);
+  print(list4);
+}
